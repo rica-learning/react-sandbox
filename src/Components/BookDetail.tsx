626ea6d76c19b5ecde62c02fc9d313d162f11161
Rica Learning <rica.learning@gmail.com>
2021-06-17 20:57:26 +0800

    update page and components to fetch from the right api

--- src/Components/BookDetail.tsx
+++ src/Components/BookDetail.tsx
@@ -1,28 +1,33 @@
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router";
-// import { Book } from "../Pages/BookList";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Book } from "../Pages/BookList";
 import React from "react";
 
 export default function AuthorDetail() {
-  // let { id } = useParams();
-  // const [data, setData] = useState({ title: "", author: "", _id: "" });
+  let { id } = useParams();
+  const [data, setData] = useState({ title: "", author: "", _id: "" });
 
-  // useEffect(() => {
-  //   fetch(`https://1fe1m.sse.codesandbox.io/api/books/${id}`)
-  //     .then((res) => res.json())
-  //     .then(setData);
-  // }, [id]);
+  useEffect(() => {
+    fetch(`https://ricaappapi.azurewebsites.net/api/books/${id}`)
+      .then((res) => res.json())
+      .then(setData);
+  }, [id]);
 
-  // let book: Book = data;
+  let book: Book = data;
 
-  return <></>;
+  return (
+  <>
+    {book.title ? (
+      <>
+        <h1> {book.title} </h1>
+        <h2> Author : {book.author} </h2>
+      </>
+    ) : (
+      <h1>Loading . . . </h1>
+    )}
+  
+  </>
+  )
 }
 
-// {book.title ? (
-//   <>
-//     <h1> {book.title} </h1>
-//     <h2> Author : {book.author} </h2>
-//   </>
-// ) : (
-//   <h1>Loading . . . </h1>
-// )}
+
